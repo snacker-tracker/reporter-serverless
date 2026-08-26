a3ae1fb825728d7675d6a43aa99c0d12dc4c499e
Laurence MacGuire <leprechaun@gmail.com>
2026-08-26 23:35:23 +0700

    simplify the docker setup

--- lambda.tf
+++ lambda.tf
@@ -87,9 +87,14 @@ resource "aws_lambda_function" "append_to_bronze" {
   function_name = "${var.api_name}-append-to-bronze-${terraform.workspace}"
   role          = aws_iam_role.lambda_execution_role.arn
   package_type  = "Image"
-  image_uri     = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.region}.amazonaws.com/snacker-tracker-lambda/append-to-bronze:${var.version_label}-append"
+  image_uri     = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.region}.amazonaws.com/snacker-tracker-lambda/append-to-bronze:${var.version_label}"
   timeout       = 300
   memory_size   = 1024
+
+  image_config {
+    command = ["append_eventbridge_to_bronze.handler.append_to_bronze"]
+  }
+
 
   environment {
     variables = {
@@ -102,9 +107,13 @@ resource "aws_lambda_function" "rebuild_bronze" {
   function_name = "${var.api_name}-rebuild-bronze-${terraform.workspace}"
   role          = aws_iam_role.lambda_execution_role.arn
   package_type  = "Image"
-  image_uri     = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.region}.amazonaws.com/snacker-tracker-lambda/append-to-bronze:${var.version_label}-rebuild"
+  image_uri     = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.region}.amazonaws.com/snacker-tracker-lambda/append-to-bronze:${var.version_label}"
   timeout       = 900
   memory_size   = 1024
+
+  image_config {
+    command = ["append_eventbridge_to_bronze.handler.rebuild_bronze"]
+  }
 
   environment {
     variables = {
